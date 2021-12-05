f1c3f81988daed9b27e6609e2d33630d52eda00b
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-12-06 00:11:09 +0800

    Add files via upload

---
+++ BalancedTournamentArmorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using SandBox;
+
+namespace BalancedTournamentArmor
+{
+    [HarmonyPatch(typeof(TournamentFightMissionController), "AddRandomClothes")]
+    public class BalancedTournamentArmorController
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].operand is MethodInfo method && method == AccessTools.Method(typeof(CharacterObject), "get_RandomBattleEquipment"))
+                {
+                    codes[i].operand = AccessTools.Method(typeof(BalancedTournamentArmorController), "RandomBattleEquipment");
+                    codes[i].opcode = OpCodes.Call;
+                    codes[i - 1].opcode = OpCodes.Nop;
+                    codes[i - 2].opcode = OpCodes.Nop;
+                }
+            }
+            return codes;
+        }
+        public static Equipment RandomBattleEquipment() => Settlement.CurrentSettlement.Culture.BasicTroop.RandomBattleEquipment;
+    }
+}
---
+++ BalancedTournamentArmorSubModule.cs
@@ -0,0 +1,10 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace BalancedTournamentArmor
+{
+    public class BalancedTournamentArmorSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad() => new Harmony("mod.bannerlord.balancedtournamentarmor").PatchAll();
+    }
+}
