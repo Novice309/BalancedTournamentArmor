5ebac1aa41afaa1f4eae5c5cc1fb5363e2cab5b7
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-12-31 23:20:53 +0800

    Update BalancedTournamentArmorSettings.cs

--- BalancedTournamentArmorSettings.cs
+++ BalancedTournamentArmorSettings.cs
@@ -1,4 +1,4 @@
-﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes;
 using MCM.Abstractions.Attributes.v2;
 using MCM.Abstractions.Settings.Base.Global;
 
@@ -10,7 +10,7 @@ namespace BalancedTournamentArmor
         public override string DisplayName => "Balanced Tournament Armor";
         public override string FolderName => "BalancedTournamentArmor";
         public override string FormatType => "json2";
-        [SettingPropertyBool("Heal Heroes to Full HP", Order = 0, RequireRestart = false, HintText = "Heal nobles and companions to full HP. HP will revert after the tournament. Enabled by default.")]
+        [SettingPropertyBool("Heal Heroes to Full HP", Order = 0, RequireRestart = false, HintText = "Heal nobles, companions and the player to full HP. HP will revert after the tournament. Enabled by default.")]
         [SettingPropertyGroup("HP", GroupOrder = 0)]
         public bool ShouldHealHeroAgents { get; set; } = true;
         [SettingPropertyBool("Wear Noble Armor", Order = 0, RequireRestart = false, HintText = "Participants wear basic noble troop armor instead of basic troop armor. Disabled by default.")]
